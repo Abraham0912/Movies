23d11bc5899f0a93d131789f3a72bbd65aa1f454
Abraham0912 <09120912ahn@gmail.com>
2021-10-17 16:57:39 -0500

    move destructurng.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { App } from "./App";
-import { Destructuring } from './components/Destructuring'
+import { Destructuring } from './destructuring/Destructuring'
 
 ReactDOM.render(
     <div>
