5e26db8d1a1c3d83d3f6f046e18e39c366158527
Abraham0912 <09120912ahn@gmail.com>
2021-10-17 23:20:05 -0500

    update routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { MoviesGrid } from "./components/MoviesGrid";
 import styles from "./App.module.css";
 import {
   BrowserRouter as Router,
@@ -7,16 +6,20 @@ import {
   Link
 } from "react-router-dom";
 import { Destructuring } from "./destructuring/Destructuring";
+import { MoviesDetails } from "./pages/MoviesDetails";
+import { MoviesGrid } from "./components/MoviesGrid";
 
 export function App() {
   return (
       <div>
         <Router>
       <header>
-        <h1 className={styles.title}>Movies</h1>
-        <Link to="/movies">Peliculas</Link>
+      <Link to="/"><h1 className={styles.title}>Movies</h1></Link>
+        <Link to="/movies">Detalle de pelicula</Link>
         <br />
         <Link to="/destructuring">Destructuracion</Link>
+        <br />
+        <Link to="/">HOME</Link>
       </header>
       <main>
       
@@ -24,11 +27,14 @@ export function App() {
           <Route path="/destructuring">
             <Destructuring title="Soy el titulo" content="soy contenido see!"/>
           </Route>
-          <Route path="/movies">
+          <Route exact path="/">
             <MoviesGrid />
           </Route>
+          <Route exact path="/movies/:id">
+            <MoviesDetails />
+          </Route>
           <Route path="/">
-            <h1>SOY HOME</h1>
+            <h1>404 NOT FOUND PAGE</h1>
           </Route>
       </Switch>
       </main>
