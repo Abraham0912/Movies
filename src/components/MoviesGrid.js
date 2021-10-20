5c90cf56d206b911d834218cf6c72c5491135ad1
Abraham0912 <09120912ahn@gmail.com>
2021-10-20 11:57:08 -0500

    refatcor

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -3,6 +3,7 @@ import styles from "./MoviesGrid.module.css";
 
 import { MovieCard } from "./MovieCard";
 import { useEffect, useState } from "react";
+import { get } from "../utils/httpClient";
 
 export function MoviesGrid() {
   const [movies, setMovies]=useState([]);
@@ -10,18 +11,11 @@ export function MoviesGrid() {
   //const movies = moviesState[0];
   //const setMovies = moviesState[0];
 
-  useEffect(()=>{
-    fetch("https://api.themoviedb.org/3/discover/movie",
-    {
-      headers: {
-        "Authorization":"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiZmU4Nzg5NGMzZDNkYjNjNGVkYWRlN2RiODA2MDAwMyIsInN1YiI6IjYxNmNjMzA5ODk0ZWQ2MDAyMzI3OTNlZSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AyinEchDCJ7NAkbswqZd-0VXwItrDs0ldMkLQVdWD1g",
-        "Content-Type": "application/json;charset=utf-8"
-      }
-    }).then(result=> result.json()).then(data =>{
-      setMovies(data.results)
-      console.log(movies)
+  useEffect(() => {
+    get("/discover/movie").then((data) => {
+      setMovies(data.results);
     });
-  },[]);
+  }, []);
   return (
     <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
