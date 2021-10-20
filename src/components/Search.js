26c2c36d12f3cc494a0c96c1b714b4f45d1511cf
Abraham0912 <09120912ahn@gmail.com>
2021-10-20 16:50:11 -0500

    create

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,28 @@
-import React from 'react'
+import { useState } from 'react'
+import styles from "./Search.module.css"
+import { FaSearch } from "react-icons/fa";
 
 export const Search = () => {
+    const [searchText,setSearchText] = useState("Hola cara de bola")
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        alert(searchText)
+    }
     return (
-        <div>
-            <form action="">
-                <input placeholder="Buscar..." type="text" />
-            </form>
-        </div>
+        <form className={styles.searchContainer} onSubmit={handleSubmit}>
+            <div className={styles.searchBox}>
+                <input
+                value={searchText}
+                onChange={(e)=>{
+                    setSearchText(e.target.value)
+                    console.log(e.target.value)
+                }} 
+                className={styles.searchInput} placeholder="Buscar..." 
+                type="text" />
+                <button className={styles.searchButton} type="submit">
+                    <FaSearch size={20}/>
+                </button>
+            </div>    
+        </form>
     )
 }
