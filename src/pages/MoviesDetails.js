b4551fb3e764741ab576851460a11f4d17657951
Abraham0912 <09120912ahn@gmail.com>
2021-10-19 22:33:44 -0500

    add css

--- src/pages/MoviesDetails.js
+++ src/pages/MoviesDetails.js
@@ -1,19 +1,22 @@
 import React from 'react'
 import movie from "./movie.json";
-import movies from "../components/movies.json";
+//import movies from "../components/movies.json";
 import styles from "./MoviesDetails.module.css"
 
 export const MoviesDetails = () => {
-    const imageUrl = "https://image.tmdb.org/t/p/w300" + movies.poster_path;
+    const imageUrl = "https://image.tmdb.org/t/p/w500" + movie.poster_path;
     return (
         <div className={styles.detailsContainer}>
-            <br />
-            <br />
-            <br />
-            <br />
-            <img src={imageUrl} alt="" />
-            Aqui deberia estar los detalles de la pelicola || RESTAURANTE
-            m
+            {/* La siguiente linea es una opcion para poner mas de un className 
+            <img className={styles.col + " " + styles.movieImage} src={imageUrl} alt={movie.title} /> */}
+            <img className={`${styles.col} ${styles.movieImage}`} src={imageUrl} alt={movie.title} />
+            <div className={styles.col + " " + styles.movieDetails}>
+                <p className={styles.firstItem}><strong>Title:</strong> {movie.title}</p>
+                <p>
+                    <strong>Generes:</strong>{movie.genres.map(genere => genere.name).join(", ")}
+                </p>
+                <p><strong>Description:</strong> {movie.overview}</p>
+            </div>
         </div>
     )
 }
