aa38e66604a1449cc6f08b7d91e3af571c235478
Abraham0912 <09120912ahn@gmail.com>
2021-10-21 18:07:13 -0500

    use useDebounce

--- src/pages/LandinPages.js
+++ src/pages/LandinPages.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { MoviesGrid } from '../components/MoviesGrid'
 import { Search } from '../components/Search'
+import { useDebounce } from '../my_hoocks/useDebounce';
 import { useQuery } from '../my_hoocks/useQuery';
 
 export const LandinPages = () => {
     const query = useQuery();
     const search = query.get("search")
+
+    const debouncedSearch = useDebounce(search,800);
     return (
         <div>
             <Search/>
-            <MoviesGrid key={search} search={search}/>
+            <MoviesGrid key={debouncedSearch} search={debouncedSearch}/>
         </div>
     )
 }
