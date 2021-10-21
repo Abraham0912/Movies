3dbca5008241b8e5d69e67f00be1665d0363cf00
Abraham0912 <09120912ahn@gmail.com>
2021-10-21 17:42:59 -0500

    add props

--- src/pages/LandinPages.js
+++ src/pages/LandinPages.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import { MoviesGrid } from '../components/MoviesGrid'
 import { Search } from '../components/Search'
+import { useQuery } from '../my_hoocks/useQuery';
 
 export const LandinPages = () => {
+    const query = useQuery();
+    const search = query.get("search")
     return (
         <div>
             <Search/>
-            <MoviesGrid/>
+            <MoviesGrid key={search} search={search}/>
         </div>
     )
 }
