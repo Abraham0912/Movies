c5e803b3b522e5d290e41ab99cbb9f8447ae66ea
Abraham0912 <09120912ahn@gmail.com>
2021-10-21 17:43:44 -0500

    scroll infinito

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -6,38 +6,53 @@ import { useEffect, useState } from "react";
 import { get } from "../utils/httpClient";
 import { Spinner } from "./Spinner";
 import { useQuery } from "../my_hoocks/useQuery";
+import InfiniteScroll from "react-infinite-scroll-component"
 
 
-export function MoviesGrid() {
+export function MoviesGrid({search}) {
   const [movies, setMovies]=useState([]);
   //Las siguientes lineas es la verdadera manera de usar el useState, arriba es usando destructuring
   //const movies = moviesState[0];
   //const setMovies = moviesState[0];
 
+  const [page,setPage]=useState(1);
+
   const [loading,setLoading] = useState(true);
 
-  const query = useQuery();
-  const search = query.get("search")
+  const [hasMore,setHasMore] = useState(true);
+
+  // const query = useQuery();
+  // const search = query.get("search")
 
   useEffect(() => {
     setLoading(true);
     const searchUrl = search
-      ? "/search/movie?query=" + search
-      : "/discover/movie";
+      ? "/search/movie?query=" + search + "&page" + page
+      : "/discover/movie?page=" + page;
     get(searchUrl).then((data) => {
-      setMovies(data.results);
+      setMovies(prevMovies => prevMovies.concat(data.results));
+      console.log(data)
+      setHasMore(data.page < data.total_pages)
       setLoading(false);
     });
   }, [search]);
 
-  if(loading){
-    return <Spinner/>
-  }
+  // if(loading){
+  //   return <Spinner/>
+  // }
   return (
+    <InfiniteScroll
+     dataLength={movies.length}
+     hasMore={hasMore}
+     next={()=>setPage((prevPage)=>prevPage +1)}//Cambiar el estado a aprtir de un estado anterior
+     //next={()=>alert("SE VA CARGAR LA SIGUIENTE PAGINA WARING")}
+     loader={<Spinner/>}
+    >
     <ul className={styles.moviesGrid}>
       {movies.map((movie) => (
         <MovieCard key={movie.id} movie={movie} />
       ))}
     </ul>
+    </InfiniteScroll>
   );
 }
